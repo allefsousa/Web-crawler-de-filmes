0714c6c34372187d65142d02e442f33866d9f781
Allef Sousa <allefsousa_1@hotmail.com>
2018-01-15 00:10:04 -0200

    Buscdefilmes

--- Spider.js
+++ Spider.js
@@ -4,5 +4,18 @@ var cheerio = require('cheerio');
 /**
  * request por padrão sem arguemnto executa um get
  * passando a url da requisicao
+ * e passando a função com os parametros de erro responsta e corpo
  */
-request("http://www.imdb.com/chart/moviemeter")
+request("http://www.imdb.com/chart/moviemeter",function(err,res,body){ 
+        if(err) console.log ('Erro :' + err);
+
+        var $ = cheerio.load(body); // recebendo o corpo da requisição
+        $('.lister-list tr').each(function(){
+            var title = $(this).find('.titleColumn a').text().trim();
+            var rating = $(this).find('.imdbRating a').text().trim();
+
+            console.log("titulo: "+title);
+        });
+
+        
+});

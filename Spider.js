56fd1b37189d6fa8f7ba74c130f0a0b829bb3963
Allef Sousa <allefsousa_1@hotmail.com>
2018-01-15 00:16:26 -0200

    adicionando filmes a arquivo txt

--- Spider.js
+++ Spider.js
@@ -1,19 +1,23 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs'); // file system 
 
 /**
  * request por padrão sem arguemnto executa um get
  * passando a url da requisicao
  * e passando a função com os parametros de erro responsta e corpo
+ * 
  */
-request("http://www.imdb.com/chart/moviemeter",function(err,res,body){ 
+request("http://www.imdb.com/chart/movgit add *iemeter",function(err,res,body){ 
         if(err) console.log ('Erro :' + err);
 
         var $ = cheerio.load(body); // recebendo o corpo da requisição
         $('.lister-list tr').each(function(){
             var title = $(this).find('.titleColumn a').text().trim();
-            var rating = $(this).find('.imdbRating a').text().trim();
+            var rating = $(this).find('.imdbRating strong').text().trim();
 
+
+            fs.appendFile("Os 100 melhores filmes.txt",title + ' ' + rating + '\n') // adicionando filmes buscados a um arquivo txt
             console.log("titulo: "+title);
         });
 
